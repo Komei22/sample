03a21604fa64e0d102d2c57d343f987ec8c8fc35
Komei Nomura <komei.nomura@pepabo.com>
2019-08-29 17:31:11 +0900

    change inputing the text as args

--- python/mecab.py
+++ python/mecab.py
@@ -1,12 +1,15 @@
 import MeCab
+import sys
 
 mecab = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+# mecab = MeCab.Tagger('-Owakati')
 
-text = '今日はとんこつラーメンが食べたい気分だな。でも味噌ラーメンもいいなあ'
-mecab.parse('')
+args = sys.argv
+text = args[1]
+
 node = mecab.parseToNode(text)
 while node:
 	word = node.surface
 	pos = node.feature.split(",")[1]
-	print('{0} , {1}'.format(word, pos))
+	print(word, node.feature)
 	node = node.next
