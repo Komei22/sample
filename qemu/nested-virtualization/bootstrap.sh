5a4dbee8b11e4465a95df305bdf537958257690b
Komei22 <softball.22.5113@gmail.com>
2021-08-19 17:32:25 +0900

    保存先の変更

--- qemu/nested-virtualization/bootstrap.sh
+++ qemu/nested-virtualization/bootstrap.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y cloud-image-utils
 sudo apt-get install -y qemu-kvm qemu-utils ovmf
 
 # ubuntu20.04イメージの取得
-wget https://cloud-images.ubuntu.com/releases/20.04/release/ubuntu-20.04-server-cloudimg-amd64.img /home/vagrant/data/ubuntu-20.04-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/releases/20.04/release/ubuntu-20.04-server-cloudimg-amd64.img /home/vagrant/ubuntu-20.04-server-cloudimg-amd64.img
